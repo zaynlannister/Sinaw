91111ed31c743f7bcd0a6f46295bf8857c4cd67d
zaynlannister <zaynlannister@gmail.com>
2022-08-07 17:18:10 +0500

    feat: update carousel

--- src/js/webinar-carousel.js
+++ src/js/webinar-carousel.js
@@ -2,14 +2,21 @@ import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
 
 
-const swiper = new Swiper('.swiper', {
-    spaceBetween: 30,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
+const swiper = new Swiper(".swiper", {
+    effect: "coverflow",
+    centeredSlides: true,
+    loop: true,
+    slidesPerView: 1.5,
+    coverflowEffect: {
+        rotate: -2,
+        stretch: "-80%",
+        depth: 200,
+        modifier: -1,
+        slideShadows: false
     },
+
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
     }
 });
