52309fc157d770d714e69b410a2ce157837f02f2
zaynlannister <zaynlannister@gmail.com>
2022-08-09 23:29:55 +0500

    remove toggle function

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -1,19 +1,15 @@
 const menuToggle = document.querySelector('#menuToggle');
 const navMenu = document.querySelector('.header__nav-links');
 
-function openOrCloseNavigationMenu() {
-    navMenu.classList.toggle('active')
-}
-
 menuToggle.addEventListener('click', function() {
     if (this.getAttribute('data-status') === 'closed') {
-        openOrCloseNavigationMenu()
-        this.setAttribute('data-status', 'opened')
-        document.body.classList.add('stop-scroll')
+        navMenu.classList.add('active');
+        this.setAttribute('data-status', 'opened');
+        document.body.classList.add('stop-scroll');
     } else {
-        openOrCloseNavigationMenu()
-        this.setAttribute('data-status', 'closed')
-        document.body.classList.remove('stop-scroll')
+        navMenu.classList.remove('active');
+        this.setAttribute('data-status', 'closed');
+        document.body.classList.remove('stop-scroll');
     }
 })
 
