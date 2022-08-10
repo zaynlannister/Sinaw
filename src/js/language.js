aa7399fe652f092097091a5c5bdd392e1d166ad5
zaynlannister <zaynlannister@gmail.com>
2022-08-10 23:07:00 +0500

    add function for rendering languages

--- src/js/language.js
+++ src/js/language.js
@@ -1,8 +1,22 @@
 const langContainer = document.querySelector('.language');
 const langDropDownMenu = document.querySelector('.language__menu');
 
+let allLanguages = ['ru', 'en'];
+
 langContainer.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
     langDropDownMenu.classList.toggle('active');
 }
+
+function renderLanguages() {
+    allLanguages.forEach(item => {
+        langDropDownMenu.innerHTML += getLanguagesTemplate(item);
+    })
+}
+
+function getLanguagesTemplate(lang) {
+    return `<a class="language__link" data-language="${lang}">${lang}</a>`
+}
+
+renderLanguages();
