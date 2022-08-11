40c7793cbd7255e2812116fa7ca7fc357a660a89
zaynlannister <zaynlannister@gmail.com>
2022-08-11 20:41:14 +0500

    feat: add translator

--- src/js/language.js
+++ src/js/language.js
@@ -1,22 +1,8 @@
-const langContainer = document.querySelector('.language');
+const langContainer = document.querySelector('.language__page');
 const langDropDownMenu = document.querySelector('.language__menu');
-
-let allLanguages = ['ru', 'en'];
 
 langContainer.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
     langDropDownMenu.classList.toggle('active');
 }
-
-function renderLanguages() {
-    allLanguages.forEach(item => {
-        langDropDownMenu.innerHTML += getLanguagesTemplate(item);
-    })
-}
-
-function getLanguagesTemplate(lang) {
-    return `<a class="language__link" data-language="${lang}">${lang}</a>`
-}
-
-renderLanguages();
